60db781738800eaac40c495c1918f10b376cc5f0
Gus <gsc13@tuta.io>
2021-07-18 18:43:25 -0300

    Implement BooksForm component, Update App component

---
+++ src/components/BooksForm.js
@@ -0,0 +1,23 @@
+const categories = [
+  'Action', 'Biography', 'History',
+  'Horror', 'Kids', 'Learning', 'Sci-fi',
+];
+
+const BooksForm = () => (
+  <form>
+    <div>
+      Title:
+      <input type="text" />
+    </div>
+    <div>
+      Select a Category:
+      <select>
+        {categories.map((category) => (
+          <option key={category.id} value={category}>{category}</option>
+        ))}
+      </select>
+    </div>
+  </form>
+);
+
+export default BooksForm;

43963e158e186f41afef9d3fc4e8e00e27ae9fa6
Gus <gsc13@tuta.io>
2021-07-19 16:22:19 -0300

    Add rootReducer, Move initialState to book reducer

--- src/index.js
+++ src/index.js
@@ -3,34 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './stylesheet/index.css';
-import { v4 as uuidv4 } from 'uuid';
 import App from './components/App';
-import booksReducer from './reducers/book';
+import rootReducer from './reducers/index';
 
-const initialState = {
-  books: [
-    {
-      ID: uuidv4(),
-      title: 'House of Sticks',
-      category: 'Biography',
-    },
-    {
-      ID: uuidv4(),
-      title: 'The Hidden Knife',
-      category: 'Kids',
-    },
-    {
-      ID: uuidv4(),
-      title: 'Survive The Night',
-      category: 'Horror',
-    },
-  ],
-};
-
-const store = createStore(
-  booksReducer,
-  initialState,
-);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,6 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/types';
 
-const booksReducer = (state = {}, action) => {
+const initialState = {
+  books: [
+    {
+      ID: uuidv4(),
+      title: 'House of Sticks',
+      category: 'Biography',
+    },
+    {
+      ID: uuidv4(),
+      title: 'The Hidden Knife',
+      category: 'Kids',
+    },
+    {
+      ID: uuidv4(),
+      title: 'Survive The Night',
+      category: 'Horror',
+    },
+  ],
+};
+
+const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOK:
       return {

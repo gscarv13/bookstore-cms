77cf107b32e01630c9edc65fa21bff06cc7e4f7c
Gus <gsc13@tuta.io>
2021-07-21 15:52:02 -0300

    Add Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,18 @@ import '../stylesheet/index.css';
 
 const App = () => (
   <div className="Bookstore-CMS">
+    <div className="Navbar">
+      <div className="Navbar-left">
+        <h1>Bookstore CMS</h1>
+        <button type="button">BOOKS</button>
+        <button type="button">CATEGORIES</button>
+      </div>
+      <div className="Navbar-right">
+        <button type="button">
+          <i className="fas fa-user" />
+        </button>
+      </div>
+    </div>
     <div className="Main">
       <BookList />
       <BooksForm />
